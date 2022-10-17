88351132c39cb6d52b124b8f693ec323d56ba5d1
Andrey Volynkin <kazakmt4@gmail.com>
2022-10-17 14:50:53 +0300

    попытка понять что надо делать

--- index.php
+++ index.php
@@ -6,11 +6,20 @@
 * create array routs
 */
 $urlList = [ '/main/' => [
-    'GET' => 'Main::list()',
-    'POST' => 'Main::add()',
-    'DELETE' => 'Main::delete($id)'
+        'GET' => 'Main::list()',
+        'POST' => 'Main::add()',
+        'DELETE' => 'Main::delete($id)'
     ],
-    '/auth/' => 'Auth::auth()'
+    '/auth/' => 'Auth::auth()',
+    '/user/' => [
+        'GET' => 'User::show()',
+        'POST' => 'User::addUser()',
+        'PUT' => 'User::updateUser()'
+    ],
+    '/user/{id}' => [
+        'GET' => 'User::showUser($id)',
+        'DELETE' => 'Main::delete($id)'
+    ],
 ];
 
 //var_dump($_SERVER['DOCUMENT_ROOT']);
