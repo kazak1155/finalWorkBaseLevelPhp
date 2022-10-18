97b91852f34e33c7c034f11cd4856cdd64b22682
Andrey Volynkin <kazakmt4@gmail.com>
2022-10-18 11:33:37 +0300

    непонятно что надо сдлеать

--- autoload.php
+++ autoload.php
@@ -1,5 +1,5 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 
-var_dump($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
+//var_dump($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
 
