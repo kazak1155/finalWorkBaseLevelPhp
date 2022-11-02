e8eadf8806478d674e7e40426d3bdd2a2a4f9e3a
Andrey Volynkin <kazakmt4@gmail.com>
2022-11-02 10:17:35 +0300

    commit подключил файлы css и JS из Bootstrap в шаблоны header  и footer v2

--- layout/base/footer.php
+++ layout/base/footer.php
@@ -1,4 +1,5 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script type="text/javascript" src="js/scripts.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
 </body>
 </html>
