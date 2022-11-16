56c7d79962aaecceff4b0147ca23129a142fdb0b
AndreyV <kazakmt4@gmail.com>
2022-11-16 20:18:46 +0300

    попытка сделать массив роутов с функциями

--- index.php
+++ index.php
@@ -7,31 +7,60 @@ use App\Controllers\MainController;
 use App\Controllers\AdminController;
 use App\Controllers\UserController;
 
-session_start();
-error_reporting(E_ALL);
-ini_set('display_errors', true);
+//session_start();
+//error_reporting(E_ALL);
+//ini_set('display_errors', true);
+//
+//require_once __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php';
+//
+//$router = new Router();
+//
+//$router->get('', [MainController::class, 'mainPage']);
+//
+//$router->get('admin/user', [AdminController::class, 'getAllUsers']);
+//$router->get('admin/user/*', [AdminController::class, 'editUserForm']);
+//$router->delete('admin/user/*', [AdminController::class, 'deleteUser']);
+//$router->put('admin/user/*', [AdminController::class, 'editUser']);
+//
+//$router->get('login', [AuthorizationController::class, 'login']);
+//$router->post('login', [AuthorizationController::class, 'login']);
+//$router->get('logout', [AuthorizationController::class, 'logout']);
+//
+//$router->get('user', [UserController::class, 'showAllUser']);
+//$router->get('user/*', [UserController::class, 'PersonalAreaUser']);
+//$router->post('user', [UserController::class, 'createUser']);
+//$router->put('user/*', [UserController::class, 'updateUser']);
+//$router->delete('user/*', [UserController::class, 'deleteUser']);
+//
+//$application = new Application($router);
+//
+//$application->run($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
 
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php';
+$urlList = [
+    'admin/user' => [
+        'get' =>
+            [UserController::class, 'showAllUser']
+    ],
+    'admin/user/*' => [
+        'delete' => 'AdminController::deleteUser',
+        'put' => 'AdminController::editUser'
+    ],
+    'user' => [
+        'get' => 'UserController::showAllUser'
+    ],
+    'user/*' => [
+        'get' => 'UserController::PersonalAreaUser',
+        'put' => 'UserController::updateUser',
+        'delete' => 'UserController::deleteUser'
+    ]
+];
 
-$router = new Router();
-
-$router->get('', [MainController::class, 'mainPage']);
-
-$router->get('admin/user', [AdminController::class, 'getAllUsers']);
-$router->get('admin/editUser/*', [AdminController::class, 'editUserForm']);
-$router->delete('admin/user/*', [AdminController::class, 'deleteUser']);
-$router->put('admin/user/*', [AdminController::class, 'editUser']);
-
-$router->get('login', [AuthorizationController::class, 'login']);
-$router->post('login', [AuthorizationController::class, 'login']);
-$router->get('logout', [AuthorizationController::class, 'logout']);
-
-$router->get('user', [UserController::class, 'showAllUser']);
-$router->get('user/*', [UserController::class, 'PersonalAreaUser']);
-$router->post('user', [UserController::class, 'createUser']);
-$router->put('user/*', [UserController::class, 'updateUser']);
-$router->delete('user/*', [UserController::class, 'deleteUser']);
-
-$application = new Application($router);
-
-$application->run($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
+foreach ($urlList as $key1 => $list1) {
+    echo '<pre>';
+    foreach ($list1 as $key2 => $list2){
+        foreach ($list2 as $key3 => $list3){
+//            echo "path= " . $key1 . " , method= " . $key2 . ' , class= ' . $list3 . '<br>';
+            var_dump($list3);
+        }
+    }
+}
