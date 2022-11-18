04ccedd9e7a74ea1f5b8138c440ab71f093b79e2
Andrey Volynkin <kazakmt4@gmail.com>
2022-11-18 14:04:18 +0300

    18_11_22 14:03

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ ini_set('display_errors', true);
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php';
 
 $router = new Router();
-//
+
 //$router->get('', [MainController::class, 'mainPage']);
 //
 //$router->get('admin/user', [AdminController::class, 'getAllUsers']);
@@ -26,38 +26,69 @@ $router = new Router();
 //$router->post('login', [AuthorizationController::class, 'login']);
 //$router->get('logout', [AuthorizationController::class, 'logout']);
 //
-$router->get('user', [UserController::class, 'showAllUser']);
-$router->get('user/*', [UserController::class, 'PersonalAreaUser']);
-$router->post('user', [UserController::class, 'createUser']);
-$router->put('user/*', [UserController::class, 'updateUser']);
-$router->delete('user/*', [UserController::class, 'deleteUser']);
+//$router->get('user', [UserController::class, 'showAllUser']);
+//$router->get('user/*', [UserController::class, 'PersonalAreaUser']);
+//$router->post('user', [UserController::class, 'createUser']);
+//$router->put('user/*', [UserController::class, 'updateUser']);
+//$router->delete('user/*', [UserController::class, 'deleteUser']);
 
 $urlList = [
     'admin/user' => [
-        'get' => 'UserController::showAllUser'
+        'get' => [
+            'UserController::class' => 'showAllUser'
+        ]
     ],
     'admin/user/*' => [
-        'delete' => 'AdminController::deleteUser',
-        'put' => 'AdminController::editUser'
+        'delete' =>  [
+            'AdminController::class' => 'deleteUser'
+        ],
+        'put' => [
+            'AdminController::class' => 'editUser'
+        ],
+        'get' => [
+            'AdminController::class' => 'PersonalAreaUser'
+        ]
     ],
     'user' => [
-        'get' => 'UserController::showAllUser'
+        'get' => [
+            'UserController::class' => 'showAllUser'
+        ]
     ],
     'user/*' => [
-        'get' => 'UserController::PersonalAreaUser',
-        'put' => 'UserController::updateUser',
-        'delete' => 'UserController::deleteUser'
+        'get' => [
+            'UserController::class' => 'PersonalAreaUser'
+        ],
+        'put' => [
+            'UserController::class' => 'updateUser'
+        ],
+        'delete' => [
+            'UserController::class' => 'deleteUser'
+        ]
+    ],
+    'login' => [
+        'get' => [
+            'AuthorizationController::class' => 'login'
+        ],
+        'post' => [
+            'AuthorizationController::class' => 'login'
+        ],
+        'get' => [
+            'AuthorizationController::class' => 'logout'
+            ]
+    ],
+    '' => [
+        'get' => [
+            'MainController::class' => 'mainPage'
+        ]
     ]
 ];
-$addTextTostring = '::class';
+
 foreach ($urlList as $key1 => $list1) {
     echo '<pre>';
     foreach ($list1 as $key2 => $list2){
-//            var_dump($key2); var_dump($list2);
-        $classFunction = explode("::", $list2);
-//        echo 'path= ' . $key1 . ' method= ' . $key2 . ' class= ' . $classFunction[0].$addTextTostring . ' function= ' . $classFunction[1];
-        $router->$key2($key1, [$classFunction[0].$addTextTostring, $classFunction[1]]);
-        echo '$router->' . $key2 . '(' . $key1 . ', [' . $classFunction[0] . ', ' .  $classFunction[1] . ']';
+        foreach ($list2 as $key3 => $list3) {
+            $router->$key2($key1, [$key3, $list3]);
+        }
     }
     echo '</pre>';
 }
