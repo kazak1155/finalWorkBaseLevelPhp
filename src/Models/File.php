775bb786674f6f4ac8352435c632007dbb4db15c
Andrey Volynkin <kazakmt4@gmail.com>
2022-12-02 15:10:22 +0300

    work

---
+++ src/Models/File.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class File
+ * @package App\Models
+ */
+class File extends Model
+{
+    protected $table = 'files';
+}
